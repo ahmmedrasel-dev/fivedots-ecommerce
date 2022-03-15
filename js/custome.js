2bb168a7175f2c0c84218ce9d085343f95875c10
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-03-15 19:22:50 +0300

    update category-shop

--- js/custome.js
+++ js/custome.js
@@ -2,5 +2,27 @@
 $(function () {
   /*==========NICE SELECT============*/
   $('#nice-select').niceSelect();
-
 })
+
+
+const dropdown = (id) => {
+  const dropdowElement = document.getElementById(id);
+  if (dropdowElement.style.display === 'block') {
+    dropdowElement.style.display = 'none'
+  } else {
+    dropdowElement.style.display = 'block'
+  }
+}
+
+const handdleBrand = () => {
+  dropdown('filter-brand');
+}
+
+const handdleColor = () => {
+  dropdown('filter-color');
+}
+
+
+const handdlePrice = () => {
+  dropdown('filter-by-price');
+}
